4e91cb65b8165a3fc183cff1ed0e19975d8524db
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-06-16 21:18:14 +0100

    Tests for each basic replacement + new planned

--- tests/testthat/test_clean_names.R
+++ tests/testthat/test_clean_names.R
@@ -1,6 +1,38 @@
 context("Clean names")
 library(utilities)
 
+test_that("each basic replacement works", {
+  # Capitals
+  expect_equal(clean_names(c("ABC")),
+               "abc")
+  # Spaces
+  expect_equal(clean_names(c("a b c")),
+               c("a_b_c"))
+  # Dashes
+  expect_equal(clean_names(c("a-b-c")),
+               c("a_b_c"))
+  # Pluses
+  expect_equal(clean_names(c("a+b+c")),
+               c("a_b_c"))
+  # Ampersands
+  expect_equal(clean_names(c("a&b&c")),
+               c("a_and_b_and_c"))
+  # Percent (with space before)
+  expect_equal(clean_names(c("a %")),
+               c("a_percent"))
+  # Percent (no space before)
+  expect_equal(clean_names(c("a%")),
+               c("a_percent"))
+  # Multiple underscores
+  expect_equal(clean_names(c("a__b___c")),
+               c("a_b_c"))
+  # Trailing underscores
+  expect_equal(clean_names(c("abc_")),
+               c("abc"))
+  # Unacceptable leading character
+  expect_equal(clean_names(c("1abc")),
+               c(".abc"))
+})
 test_that("multiple underscores are reduced to one", {
   expect_equal(clean_names(c("a___")), "a_")
   expect_equal(clean_names(c("and___and_and__and___and")), "and_and_and_and_and")
