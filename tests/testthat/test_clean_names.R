e0b02252d52f7bdacd092ca751cdb4c62410beb0
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-07-05 14:19:26 +0100

    Clarified tests

--- tests/testthat/test_clean_names.R
+++ tests/testthat/test_clean_names.R
@@ -2,42 +2,39 @@ context("Clean names")
 library(utilities)
 
 test_that("each basic replacement works", {
-  # Capitals
+  # Upper case to lower case
   expect_equal(clean_names(c("ABC")),
                "abc")
-  # Spaces
+  # Spaces to underscores
   expect_equal(clean_names(c("a b c")),
                c("a_b_c"))
-  # Dashes
+  # Dashes to underscores
   expect_equal(clean_names(c("a-b-c")),
                c("a_b_c"))
-  # Pluses
+  # Pluses to underscores
   expect_equal(clean_names(c("a+b+c")),
                c("a_b_c"))
-  # Ampersands
+  # Ampersands to "_and_"
   expect_equal(clean_names(c("a&b&c")),
                c("a_and_b_and_c"))
-  # Percent (with space before)
-  expect_equal(clean_names(c("a %")),
+  # Percent (with underscore before) to "_percent"
+  # (spaces will have changed to underscores from earlier)
+  expect_equal(clean_names(c("a_%")),
                c("a_percent"))
-  # Percent (no space before)
+  # Percent (no space before) to "_percent" (adding the underscore)
   expect_equal(clean_names(c("a%")),
                c("a_percent"))
-  # Multiple underscores
+  # Reduce multiple underscores to single
   expect_equal(clean_names(c("a__b___c")),
                c("a_b_c"))
-  # Trailing underscores
+  # Remove trailing underscores
   expect_equal(clean_names(c("abc_")),
                c("abc"))
-  # Unacceptable leading character
+  # Unacceptable leading character fixed by make.names
   expect_equal(clean_names(c("1abc")),
                c("x1abc"))
   expect_equal(clean_names(c("@a")),
                c("x.a"))
-})
-test_that("multiple underscores are reduced to one", {
-  expect_equal(clean_names(c("a___b")), "a_b")
-  expect_equal(clean_names(c("and___and_and__and___and")), "and_and_and_and_and")
 })
 test_that("an underscore is added before percent where appropriate", {
   expect_equal(clean_names(c("and 10%")), "and_10_percent")
